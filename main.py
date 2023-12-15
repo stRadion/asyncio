9825340360652f0a8669131e9742ddd2f67ad5b9
Radion <st.radion@gmail.com>
2023-12-15 16:19:25 +0600

    3

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import asyncio
-from asyncio import CancelledError
-from util import delay
+import requests
+from util import async_timed
+@async_timed()
+async def get_example_status() -> int:
+    return requests.get('http://www.example.com').status_code
 
+@async_timed()
 async def main():
-    long_task = asyncio.create_task(delay(10))
-    seconds_elapsed = 0
-    while not long_task.done():
-        print('Задача не закончилась, следующая проверка через секунду.')
-        await asyncio.sleep(1)
-        seconds_elapsed = seconds_elapsed + 1
-        if seconds_elapsed == 5:
-            long_task.cancel()
-    try:
-        await long_task
-    except CancelledError:
-        print('Наша задача была снята')
-
+    task_1 = asyncio.create_task(get_example_status())
+    task_2 = asyncio.create_task(get_example_status())
+    task_3 = asyncio.create_task(get_example_status())
+    await task_1
+    await task_2
+    await task_3
 asyncio.run(main())
